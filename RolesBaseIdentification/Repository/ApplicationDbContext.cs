374ed5a582236ccc19af31d67e95f329e210f34b
nirajuprety <nerajupreti@gmail.com>
2024-12-23 23:37:19 +0545

    intial snapshot

---
+++ RolesBaseIdentification/Repository/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RolesBaseIdentification.Repository
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext()
+        {
+            
+        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+            AppContext.SetSwitch("Npgsql.DisableDateTimeInfinityConversions", true);
+        }
+    }
+}
---
+++ RolesBaseIdentification/Seeder/RoleSeeder.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace RolesBaseIdentification.Seeder
+{
+    public class RoleSeeder
+    {
+        public static async Task SeedRolesAsync(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            var roles = new[] { "Super Admin", "Staff", "Low-Level Staff", "User" };
+
+            foreach (var role in roles)
+            {
+                if (!await roleManager.RoleExistsAsync(role))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+        }
+    }
+}
